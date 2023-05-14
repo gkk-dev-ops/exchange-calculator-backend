6d7ed24979529ac0f4140dd432e2eda23b4056a9
gkk <gkk.dev.ops@gmail.com>
2023-05-14 16:05:42 +0200

    Move /api to seprate file and restore /api-docs endpoint

--- app.js
+++ app.js
@@ -1,86 +1,18 @@
+const apiRouter = require("./routes/api");
+const swaggerUi = require("swagger-ui-express");
 const yaml = require("yamljs");
 const express = require("express");
-const axios = require("axios");
-const availableCurrencies = require("./config/availableCurrencies.json");
-const utils = require("./utils/utils");
 
 const app = express();
 const port = 3000;
 const swaggerDocument = yaml.load("docs/swagger.yml");
 
-// Serve static files from the 'public' directory
 app.use(express.static("public"));
 
-app.get("/api/availableCurrencies", (req, res) => {
-  res.status(200).send(availableCurrencies);
-});
+app.use("/api-docs", swaggerUi.setup(swaggerDocument));
 
-app.get("/api/exchange/:currency/:currency2", async (req, res) => {
-  const { currency, currency2 } = req.params;
-  let baseCurrencyData, targetCurrencyData;
+app.use("/api", apiRouter);
 
-  if (currency === "PLN") {
-    baseCurrencyData = {
-      currency: "Polski Złoty",
-      code: "PLN",
-      mid: 1,
-    };
-  }
-  if (currency2 === "PLN") {
-    targetCurrencyData = {
-      currency: "Polski Złoty",
-      code: "PLN",
-      mid: 1,
-    };
-  }
-  try {
-    const [tableAData, tableBData] = await Promise.all([
-      axios.get("https://api.nbp.pl/api/exchangerates/tables/A?format=json"),
-      axios.get("https://api.nbp.pl/api/exchangerates/tables/B?format=json"),
-    ]);
-
-    const currenciesData = [
-      ...tableAData.data[0].rates,
-      ...tableBData.data[0].rates,
-    ];
-
-    if (!baseCurrencyData) {
-      baseCurrencyData = currenciesData.find(
-        (currencyData) => currencyData.code === currency
-      );
-    }
-    if (!targetCurrencyData) {
-      targetCurrencyData = currenciesData.find(
-        (currencyData) => currencyData.code === currency2
-      );
-    }
-
-    if (!baseCurrencyData || !targetCurrencyData) {
-      res.status(404).json({ error: "Currency not found" });
-      return;
-    }
-
-    const exchangeRate = utils.calculateExchangeRate(
-      baseCurrencyData,
-      targetCurrencyData
-    );
-
-    // Prepare the response data
-    const data = {
-      currency: currency,
-      currency2: currency2,
-      exchangeRate: exchangeRate,
-    };
-
-    // Send the response back to the client
-    res.json(data);
-  } catch (error) {
-    console.error("Error processing exchange rate:", error);
-    res.status(500).json({ error: "Failed to process exchange rate" });
-  }
-});
-
-// Custom 404 page
 app.use((req, res) => {
   res.status(404).sendFile("404.html", { root: "public" });
 });
---
+++ routes/api.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const router = express.Router();
+
+const axios = require("axios");
+const availableCurrencies = require("../config/availableCurrencies.json");
+const utils = require("../utils/utils");
+
+router.get("/availableCurrencies", (req, res) => {
+  res.status(200).send(availableCurrencies);
+});
+
+router.get("/exchange/:currency/:currency2", async (req, res) => {
+  const { currency, currency2 } = req.params;
+  let baseCurrencyData, targetCurrencyData;
+
+  if (currency === "PLN") {
+    baseCurrencyData = {
+      currency: "Polski Złoty",
+      code: "PLN",
+      mid: 1,
+    };
+  }
+  if (currency2 === "PLN") {
+    targetCurrencyData = {
+      currency: "Polski Złoty",
+      code: "PLN",
+      mid: 1,
+    };
+  }
+  try {
+    const [tableAData, tableBData] = await Promise.all([
+      axios.get("https://api.nbp.pl/api/exchangerates/tables/A?format=json"),
+      axios.get("https://api.nbp.pl/api/exchangerates/tables/B?format=json"),
+    ]);
+
+    const currenciesData = [
+      ...tableAData.data[0].rates,
+      ...tableBData.data[0].rates,
+    ];
+
+    if (!baseCurrencyData) {
+      baseCurrencyData = currenciesData.find(
+        (currencyData) => currencyData.code === currency
+      );
+    }
+    if (!targetCurrencyData) {
+      targetCurrencyData = currenciesData.find(
+        (currencyData) => currencyData.code === currency2
+      );
+    }
+
+    if (!baseCurrencyData || !targetCurrencyData) {
+      res.status(404).json({ error: "Currency not found" });
+      return;
+    }
+
+    const exchangeRate = utils.calculateExchangeRate(
+      baseCurrencyData,
+      targetCurrencyData
+    );
+
+    // Prepare the response data
+    const data = {
+      currency: currency,
+      currency2: currency2,
+      exchangeRate: exchangeRate,
+    };
+
+    // Send the response back to the client
+    res.json(data);
+  } catch (error) {
+    console.error("Error processing exchange rate:", error);
+    res.status(500).json({ error: "Failed to process exchange rate" });
+  }
+});
+
+module.exports = router;
