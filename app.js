9a3051b6d273625025866219be32eb052b087bdc
gkk <gkk.dev.ops@gmail.com>
2023-05-15 06:20:54 +0200

    Add cors and 404 site favicon

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const router = require("./routes/index");
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require('cors');
 
 const app = express();
 const port = dotenv.config().parsed.PORT || 3000;
+app.use(cors());
 
 app.use(express.static("public"));
 app.use('/', router);
